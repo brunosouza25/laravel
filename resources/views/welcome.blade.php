7a5f00208a439e51a89c3ee996e61fff304c1fa4
brunosouza25 <brunopw25@gmail.com>
2021-09-30 23:31:31 -0300

    Just following the course

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -21,6 +21,32 @@
         </style>
     </head>
     <body>
+        @if(10 > 5)
+            <h2>true</h2>
+        @endif
+        
+        @if($name == 'bruno')
+        <p>o nome é bruno</p>
+        <p>e tem: {{ $age }}</p>
+        <p>e ele é {{ $profession }}</p>
+        @else
+        <p>não é bruno</p>
+        @endif
 
+        @for($i = 0; $i < count($array); $i++)
+            <p> {{ $array[$i] }} </p> 
+        @endfor
+
+        @php
+            $name = 'leandra';
+            echo $name;
+        @endphp
+        <!-- comentário html -->
+        {{-- comentario blade --}}
+
+        @foreach($names as $name)
+            <p>{{ $loop->index }}</p>
+            <p>{{ $name }}</p>
+        @endforeach
     </body>
 </html>
--- routes/web.php
+++ routes/web.php
@@ -14,5 +14,17 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
+    $name = 'bruno';
+    $age = 23;
+    $profession = 'developer';
+
+    $array = [1, 2, 3, 4, 5];
+
+    $names = ['bruno', 'leandra', 'maria', 'josé'];
+
+    return view('welcome', ['name' => $name, 'age' => $age, 'profession' => $profession, 'array' => $array, 'names' => $names]);
+});
+
+Route::get('/contact', function () {
     return view('welcome');
 });
